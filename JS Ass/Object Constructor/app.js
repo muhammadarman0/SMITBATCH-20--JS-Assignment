c79d615fb67c1b0da3f98abf65284d37d25b3d12
muhammadarman0 <armandevs74@gmail.com>
2026-06-06 21:29:03 +0500

    inital commit

---
+++ JS Ass/Object Constructor/app.js
@@ -0,0 +1,35 @@
+// function Student(name,age,rollNo){
+//     this.name = name;
+//     this.age = age;
+//     this.rollNo = rollNo
+// }
+
+// let std1 = new Student("Ali", 13, 4)
+// let std2 = new Student("Ahmed", 14, 5)
+// let std3 = new Student("Sara", 12, 6)
+
+
+// console.log(std1)
+// console.log(std2)
+// console.log(std3)
+
+
+// function Book(tittle,author,year){
+//     this.tittle = tittle;
+//     this.author = author
+//     this.year = year
+// }
+
+// let b1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+
+// console.log(b1)
+
+function Car(color,model,usesYear){
+    this.color = color;
+    this.model = model;
+    this.usesYear = usesYear
+}
+
+let c1 = new Car("Red", "Toyota",5)
+
+console.log(c1)
