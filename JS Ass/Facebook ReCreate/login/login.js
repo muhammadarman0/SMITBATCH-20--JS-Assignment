66b8034394d028995a028bb8103a719b5ee8dc55
muhammadarman0 <armandevs74@gmail.com>
2026-06-18 15:37:14 +0500

    initial commit

---
+++ JS Ass/Facebook ReCreate/login/login.js
@@ -0,0 +1,60 @@
+let Useremail = document.getElementById("email");
+let Userpassword = document.getElementById("password")
+
+let loginBtn = document.getElementById("loginBtn")
+
+function sweety(error, tittle) {
+    Swal.fire({
+        icon: `${error}`,
+        title: `${tittle}`,
+        // text: `${text}`,
+
+    });
+}
+let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+const loginUser = () => {
+    if (Useremail.value.trim() === "") {
+        sweety("error", "Enter a email")
+        return;
+    }
+    if (Userpassword.value.trim() === "") {
+        sweety("error", "Enter a Password")
+        return
+    }
+    if (!emailRegex.test(Useremail.value.trim())) {
+        sweety("error", "Please enter a valid email")
+        return
+    }
+
+    let SignUpUser = JSON.parse(localStorage.getItem("SignUpUsers")) || []
+
+    console.log(SignUpUser[0].Useremail);
+    
+    let CurrentUser = false
+
+    for (let i = 0; i < SignUpUser.length; i++) {
+       let user = SignUpUser[i]
+
+       if(user?.Useremail === Useremail.value){
+        CurrentUser = true
+        if(user?.UserPass = Userpassword.value){
+    sweety("success","User login successfully")
+        }
+       }
+
+    }
+    // let itExistUser = false
+    // if (SignUpUser.Useremail === Useremail.value) {
+    //     itExistUser = true
+    //     if (itExistUser && SignUpUser.UserPass === Userpassword.value){
+    //         console.log("user login"); 
+    //     }
+    // }
+
+    // sweety("success","User login successfully")
+
+}
+
+
+loginBtn.addEventListener("click", loginUser)
