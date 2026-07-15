febf0428272cef09094108059d807906d5338019
muhammadarman0 <armandevs74@gmail.com>
2026-07-15 19:06:15 +0500

    initial commit

---
+++ Advance JS project/FireBase authentication/app.js
@@ -0,0 +1,11 @@
+// const arr = [1, 2, 3];
+
+// const result = arr.forEach(num => num * 2);
+
+// console.log(result);
+
+const numbers = [5, 10, 15];
+
+const result = numbers.find(num => num > 20);
+
+console.log(result);
