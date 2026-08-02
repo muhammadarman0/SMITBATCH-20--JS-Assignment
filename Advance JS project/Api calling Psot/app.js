0bfcbb12654a9df7dccf1f5421e3395b25f8204f
muhammadarman0 <armandevs74@gmail.com>
2026-08-02 19:50:18 +0500

    initial

--- Advance JS project/Api calling Psot/app.js
+++ Advance JS project/Api calling Psot/app.js
@@ -6,6 +6,9 @@ const loadApp = document.querySelector(".loader")
 const postList = document.getElementById("postList")
 const sidebar = document.querySelector(".sidebar")
 const postSection = document.querySelector(".post-section")
+const postTitle = document.querySelector(".post-title")
+const postBody = document.querySelector(".post-body")
+const searchInput = document.getElementById("searchInput")
 // const reloadAPP = () => {
 // setTimeout(()=>{
 //     loadApp.style.display = "none"
@@ -35,7 +38,6 @@ const apiCall = async (e) => {
         sideBar.innerHTML = showUI.join(" ")
     } catch (error) {
         // postList.innerHTML = `<h1 style="color:red;">Not Connect!</h1>`
-        loadApp.style.display = "flex"
         navbar.style.display = "none"
         sidebar.style.display = "none"
         postSection.style.display = "none"
@@ -49,17 +51,55 @@ apiCall()
 // console.log(getItem);
 let getId = JSON.parse(localStorage.getItem("id"))
 
-const showPostHTML = async (getId) => {
+const searchHandler = async () => {
     try {
-        let apiData = await fetch(`https://dummyjson.com/posts/${getId}`)
-        let resp = await apiData.json()
-        
-        
+        let value = searchInput.value
+        let searchApi = await fetch(`https://dummyjson.com/posts/search?q=${value}`)
+        let resp = await searchApi.json()
+        const { posts } = resp
+        if (value.trim() === "") {
+            alert("Search Each post")
+            return
+        }
+        const filterHTML = posts.filter((search) => {
+            let id = search.id
+
+            return search.body.toLowerCase().includes(value.toLowerCase()) || search.body.toLowerCase() === value.toLowerCase()
+
+        })
+        const showSearchHtml = filterHTML.map((search) => {
+            let id = search.id
+
+            return `    <div class="post-item dummy"  id="${id}">
+          <h2 class="dummy" id="${id}">${search.title}</h2>
+                   <p class="dummy"  id="${id}"> ${search.body.slice(0, 140)}</p>
+        </div>
+`
+        })
+        sideBar.innerHTML = `<h1>Post Not Found</h1>`
+        sideBar.innerHTML = showSearchHtml.join(" ")
     } catch (error) {
+        console.log(error);
 
     }
 }
 
+const showPostHTML = async (getId) => {
+    try {
+        let apiData = await fetch(`https://dummyjson.com/posts/${getId}`)
+        let resp = await apiData.json()
+        console.log(resp);
+        postTitle.innerHTML = resp.title;
+        postBody.innerHTML = resp.body
+
+    } catch (error) {
+        loadApp.style.display = "flex"
+
+    }
+}
+
+
+searchBtn.addEventListener("click", searchHandler)
 document.addEventListener("click", (e) => {
     let elem = e.target
     if (elem.classList.contains("dummy")) {
